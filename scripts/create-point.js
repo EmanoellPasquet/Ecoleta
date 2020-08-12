ec506cf81b2b543e66e6ddf6caf0bf9473ba867e
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-12 16:41:49 -0300

    advancing on JS logic for selectors

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -41,3 +41,38 @@ function getCities(event) {
 }
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+//items de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+let selectedItem = [];
+const collectedItems = document.querySelector("input[name=items]")
+
+function handleSelectedItem(event) {
+  //add or remove a class with JS
+  const itemLi = event.target;
+  const itemID = itemLi.dataset.id;
+  itemLi.classList.toggle("selected");
+
+  const alreadySelected = selectedItem.findIndex(item=>{
+    const itemExists = item == itemID //true or false
+    return itemExists
+  })
+  
+  if(alreadySelected >= 0){//if already selected, pops out from the array
+    const filteredItems = selectedItem.filter((item)=>{
+      const differentItem = item != itemID
+      return differentItem
+    })
+    selectedItem = filteredItems
+
+  }else{//if not selected pushes in to the array
+    selectedItem.push(itemID)
+  }
+  collectedItems.value = selectedItem
+}
