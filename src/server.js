65e36ce6437140f1e2497ab1911795124a8b7918
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-14 21:50:48 -0300

    creating components with nunjucks

--- src/server.js
+++ src/server.js
@@ -4,15 +4,23 @@ const server = express()
 //public folder config
 server.use(express.static("public"))
 
+//template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+   express: server,
+   noCache: true
+})
+
+
 //routes config
 server.get("/",  (req, res)=>{
-   res.sendFile(__dirname + "/views/index.html")
+   return res.render("index.html")
 } )
 server.get("/create-point",  (req, res)=>{
-   res.sendFile(__dirname + "/views/create-point.html")
+   return res.render("create-point.html")
 } )
-server.get("/search-results",  (req, res)=>{
-   res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search",  (req, res)=>{
+   return res.render("search-results.html")
 } )
 
 //turn server on
