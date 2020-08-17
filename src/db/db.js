c46b046882cf71ba743cb93e09faf5b913b0fa5a
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-17 13:36:02 -0300

    configuring database

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,84 @@
 //importing sqlite3 dependency
 
-const sqlite3 = require('sqlite3').verbose()
+const sqlite3 = require("sqlite3").verbose();
 
 // creating object to manipulate db
 
-const db = new sqlite3.Database("src/db/database.db")
+const db = new sqlite3.Database("src/db/database.db");
+
+module.exports = db
 
 //operating db object to manipulate db
+
+/*db.serialize(() => {
+  // creating a table
+  db.run(`
+      CREATE TABLE IF NOT EXISTS location (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         image TEXT, 
+         name TEXT,
+         adress TEXT, 
+         adress2 TEXT, 
+         state, TEXT,
+         city TEXT,
+         items TEXT
+      );
+   `);
+
+  //inserting data
+  const query = `
+         INSERT INTO location (
+            image,
+            name,
+            adress,
+            adress2,
+            state, 
+            city, 
+            items
+
+         ) VALUES ( ?, ?, ?, ?, ?, ?, ?);
+            `
+
+  const values = [
+          "https://images.unsplash.com/photo-1555664424-778a1e5e1b48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+          "Colectoria",
+           "Guilherme Gemballa, Jardim América",
+           "Nº 260",
+           "Santa Catarina",
+           "Rio do Sul",
+           "Resíduos Eletrônicos, Lâmpadas"
+   ]
+   
+   function AfterInsert(err){
+      if (err) {
+         return console.log(err)
+      }
+      console.log("Sucess!")
+      console.log(this)
+   }
+
+   db.run(query, values, AfterInsert)
+
+ //consulting data
+ 
+
+ db.all(`SELECT * FROM location`, function(err, rows){
+    if (err) {
+       return console.log(err)
+    }
+    console.log("Persisted data: ")
+    console.log(rows)
+ })
+
+  //deleting data
+   /*db.run(`DELETE FROM location WHERE id = ?`, [4], function(err, rows){
+   if (err){
+      return console.log(err)
+     }
+    console.log("Deleted successfully")
+    console.log(rows)
+
+
+ } )*/
+
+//});
--- src/server.js
+++ src/server.js
@@ -1,27 +1,40 @@
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
+
+//get database
+
+const db = require("./db/db.js");
 
 //public folder config
-server.use(express.static("public"))
+server.use(express.static("public"));
 
 //template engine
-const nunjucks = require("nunjucks")
+const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
-   express: server,
-   noCache: true
-})
-
+  express: server,
+  noCache: true,
+});
 
 //routes config
-server.get("/",  (req, res)=>{
-   return res.render("index.html")
-} )
-server.get("/create-point",  (req, res)=>{
-   return res.render("create-point.html")
-} )
-server.get("/search",  (req, res)=>{
-   return res.render("search-results.html")
-} )
+server.get("/", (req, res) => {
+  return res.render("index.html");
+});
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+server.get("/search", (req, res) => {
+  //getting data from database
+
+  db.all(`SELECT * FROM location`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Persisted data: ");
+    console.log(rows);
+    //show html with data from database
+    return res.render("search-results.html", { location: rows });
+  });
+});
 
 //turn server on
-server.listen(3000)
+server.listen(3000);
