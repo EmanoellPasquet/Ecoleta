d7a92e0c31b05373846a9114efcae4ccb53418b0
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-08-17 15:21:50 -0300

    :v: sqlite functional

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ const db = require("./db/db.js");
 //public folder config
 server.use(express.static("public"));
 
+// implementing req.body to use method POST
+
+server.use(expre.urlencoded({extended: true}));
+
 //template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
@@ -20,8 +24,52 @@ server.get("/", (req, res) => {
   return res.render("index.html");
 });
 server.get("/create-point", (req, res) => {
+//req.query to get data from the application with GET method (Query String)
+
   return res.render("create-point.html");
 });
+
+server.post("/create-point",(req, res)=>{
+
+//req.body to get data from the application with POST method
+//insert data on db via application
+
+  const query = `
+         INSERT INTO location (
+            image,
+            name,
+            adress,
+            adress2,
+            state, 
+            city, 
+            items
+
+         ) VALUES ( ?, ?, ?, ?, ?, ?, ?);
+            `
+
+  const values = [
+        req.body.image,
+        req.body.name,
+        req.body.adress,
+        req.body.adress2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+   ]
+   
+   function AfterInsert(err){
+      if (err) {
+         return console.log(err)
+      }
+      console.log("Sucess!")
+      console.log(this)
+      return res.render("create-point",{saved: true})
+   }
+
+   db.run(query, values, AfterInsert)
+
+})
+
 server.get("/search", (req, res) => {
   //getting data from database
 
@@ -29,10 +77,9 @@ server.get("/search", (req, res) => {
     if (err) {
       return console.log(err);
     }
-    console.log("Persisted data: ");
-    console.log(rows);
+    const total = rows.lenght
     //show html with data from database
-    return res.render("search-results.html", { location: rows });
+    return res.render("search-results.html", { location: rows, total });
   });
 });
 
